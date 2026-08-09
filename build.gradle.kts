fa25761e96568989321602abce35f8aa1ff79c48
7KSA7 <qsamalsamy59@gmail.com>
2026-08-09 23:50:30 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,9 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.4.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
-    }
-}
-
+// Root build.gradle.kts
 plugins {
     id("com.android.application") version "8.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
